776bee0a7c59455d5632c8e22cadc77f7fe32eee
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-05-17 20:11:47 +0530

    Data Science

--- Python Core/Lecture1.py
+++ Python Core/Lecture1.py
@@ -1,1 +1,30 @@
 print("Hello World!")
+
+#Remove new Line
+print("Hello", "World", sep="-", end="!!!\n") 
+
+num = 10
+print(num)
+
+pie = 3.1416
+print(pie)
+
+# user input
+fname = input()
+lname = input()
+print(fname,lname)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(name,age)
+
+# how to check data types
+print(type(10))          # <class 'int'>
+print(type(3.14))        # <class 'float'>
+print(type("Hello"))     # <class 'str'>
+print(type([1, 2, 3]))   # <class 'list'>
+
+# type conversion
+a = 5
+b = float(a)  # Explicit
+c = 3.0 + a   # Implicit
---
+++ Python Core/Lecture2.py
@@ -0,0 +1,37 @@
+# If-Else
+age = 18
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You are too young to vote.")
+
+
+# Ternery Operator
+status = "Adult" if age >= 18 else "Minor"
+print(status)
+# Output: Adult
+
+
+
+# Nested If-Else
+marks = 85
+if marks >= 60:
+    if marks >= 80:
+        print("Grade: A")
+    else:
+        print("Grade: B")
+else:
+    print("Grade: C")
+
+
+
+# While loop
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+
+# For Loop
+for i in range(5):
+    print(i)
