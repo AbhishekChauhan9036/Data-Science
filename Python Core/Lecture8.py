470de9ec458fdee906b2e094db4dffa38f94cc45
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2025-05-18 11:36:37 +0530

    MU

---
+++ Python Core/Lecture8.py
@@ -0,0 +1,22 @@
+# Modules in Python
+import Lecture8_2
+
+print(Lecture8_2.add(10, 5))        # Output: 15
+print(Lecture8_2.subtract(10, 5))   # Output: 5
+
+
+
+# Packages in Python
+import Lecture8_2
+
+print(Lecture8_2.calculate_tax(1000))   # Output: 150.0
+
+
+
+
+# The __main__ Block
+def greet():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    greet()
---
+++ Python Core/Lecture8_2.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def calculate_tax(amount):
+    return amount * 0.15
